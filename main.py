fe3a42bd42ce87b1e32432728a93e90b084730dc
Marcos Minond <minond.marcos@gmail.com>
2026-07-21 14:52:11 -0600

    Fixing attr fetching

--- main.py
+++ main.py
@@ -53,10 +53,12 @@ def main(queries: list[str]):
 """
             output.append(heading)
         title = card["name"]
-        if "flavor_name" in card:
+        if card.get("flavor_name"):
             title = f"{card['flavor_name']} #text(gray)[({title})]"
-        if card["lang"] != "en" and "printed_name" in card:
+        if card["lang"] != "en" and card.get("printed_name"):
             title = f"{title} #text(gray)[({card['printed_name']})]"
+        if card.get("prices", {}).get("usd"):
+            title = f"{title} #text(gray)[(\\${card['prices']['usd']})]"
         output.append(f"- [ ] #text(gray)[\\#{str(card['collector_number']).zfill(3)}] {title}")
 
     print(f"""
